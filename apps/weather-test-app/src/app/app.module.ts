0301bbb94cf7f4f2e4c4b8a6a3a6e7cdbb244926
Mohamed Achraf Abdelbari <med.achraf.abdelbari@gmail.com>
2021-12-04 18:15:19 +0100

    [TEST-PROJECT] Workspace prepared

--- apps/weather-test-app/src/app/app.module.ts
+++ apps/weather-test-app/src/app/app.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PreloadAllModules, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {
+      preloadingStrategy: PreloadAllModules,
+      scrollPositionRestoration: 'enabled',
+      initialNavigation: 'enabledBlocking'
+    }),
+    BrowserAnimationsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
